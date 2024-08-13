ccbc83da7c712d945a58a97f641daad4b910311b
Rafael Rios <108680246+RafaelRaios@users.noreply.github.com>
2024-08-13 19:23:35 -0300

    Update func_trigger.sql

--- func_trigger.sql
+++ func_trigger.sql
@@ -82,3 +82,48 @@ BEGIN
     END IF;
 END //
 Delimiter;
+
+--ver os filmes q aquela pessoa assitiu
+CREATE OR REPLACE FUNCTION filmes_assistidos_pelo_cliente(p_cliente_cpf CHAR)
+RETURN SYS_REFCURSOR
+IS
+    v_cursor SYS_REFCURSOR;
+BEGIN
+    OPEN v_cursor FOR
+    SELECT
+        c.nome AS cliente_nome,
+        f.Titulo AS filme_titulo,
+        s.data_ AS sessao_data
+    FROM
+        Ingresso i
+    JOIN
+        Cliente c ON i.cliente_cpf = c.CPF
+    JOIN
+        Sessao s ON i.sessao_id = s.ID AND i.filme_id = s.filme_id AND i.sala_numero = s.sala_numero AND i.cinema_id = s.cinema_id
+    JOIN
+        Filme f ON s.filme_id = f.ID
+    WHERE
+        c.CPF = p_cliente_cpf;
+
+    RETURN v_cursor;
+END;
+/
+
+-- utilizar essa funcao
+DECLARE
+    v_cursor SYS_REFCURSOR;
+    v_nome CLIENTE.NOME%TYPE;
+    v_titulo FILME.TITULO%TYPE;
+    v_data SESSAO.DATA_%TYPE;
+BEGIN
+    v_cursor := filmes_assistidos_pelo_cliente('13230942450');
+
+    LOOP
+        FETCH v_cursor INTO v_nome, v_titulo, v_data;
+        EXIT WHEN v_cursor%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE('Nome: ' || v_nome || ', Título: ' || v_titulo || ', Data: ' || TO_CHAR(v_data, 'YYYY-MM-DD'));
+    END LOOP;
+
+    CLOSE v_cursor;
+END;
+/
